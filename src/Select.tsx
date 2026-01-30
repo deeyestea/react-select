9d873db839b55c8cd9eafe671f4853aa7f79cfe9
deeyeatea <40225785+deeyestea@users.noreply.github.com>
2026-01-30 07:38:25 +0530

    Added multiselect and keyboard events

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,45 +1,121 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState, type KeyboardEvent } from 'react';
 import Styles from './Select.module.css';
 
-type SelectOption = {
+export type SelectOption = {
     label: string;
     value: string | number;
 };
 
-type SelectProps = {
-    value?: SelectOption;
-    onChange: (value: SelectOption | undefined) => void;
-    options: SelectOption[];
+type MultiSelectProps = {
+    multiple: true;
+    value?: SelectOption[];
+    onChange: (value: SelectOption[]) => void;
 };
 
-const Select = ({ value, onChange, options }: SelectProps) => {
+type SingleSelectProps = {
+    multiple?: false;
+    value?: SelectOption;
+    onChange: (value: SelectOption | undefined) => void;
+};
+
+type SelectProps = {
+    options: SelectOption[];
+} & (SingleSelectProps | MultiSelectProps);
+
+const Select = ({ multiple, value, onChange, options }: SelectProps) => {
     const [isOpen, setOpen] = useState<Boolean>(false);
     const [hightLightedIndex, setHighlightedIndex] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const clearOptions = () => {
-        onChange(undefined);
+        multiple ? onChange([]) : onChange(undefined);
     };
 
     const selectOption = (option: SelectOption) => {
-        if (option !== value) onChange(option);
+        if (multiple) {
+            const currentValue = value ?? [];
+            if (currentValue?.includes(option)) {
+                onChange(currentValue.filter((o) => o !== option));
+            } else {
+                onChange([...currentValue, option]);
+            }
+        } else {
+            if (option !== value) onChange(option);
+        }
     };
 
     const isOptionSelected = (option: SelectOption) => {
-        return option === value;
+        return multiple ? value?.includes(option) : option === value;
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        switch (e.code) {
+            case 'Enter':
+            case 'Space': {
+                e.preventDefault();
+                setOpen((prev) => !prev);
+                if (isOpen) {
+                    selectOption(options[hightLightedIndex]);
+                }
+                break;
+            }
+
+            case 'ArrowUp':
+            case 'ArrowDown': {
+                e.preventDefault();
+                if (!isOpen) {
+                    setOpen(true);
+                    break;
+                }
+
+                const newIndex =
+                    hightLightedIndex + (e.code === 'ArrowDown' ? 1 : -1);
+
+                if (newIndex >= 0 && newIndex < options.length) {
+                    setHighlightedIndex(newIndex);
+                }
+                break;
+            }
+
+            case 'Escape':
+                setOpen(false);
+        }
     };
 
     useEffect(() => {
         if (isOpen) setHighlightedIndex(0);
     }, [isOpen]);
 
+    console.log('value', value);
+
     return (
         <div
+            ref={containerRef}
+            onKeyDown={handleKeyDown}
             onBlur={() => setOpen(false)}
             onClick={() => setOpen((prev) => !prev)}
             tabIndex={0}
             className={Styles.container}
         >
-            <span className={Styles.value}>{value?.label}</span>
+            <span className={Styles.value}>
+                {multiple
+                    ? value?.map((v) => (
+                          <button
+                              key={v.value}
+                              onClick={(e) => {
+                                  e.stopPropagation();
+                                  selectOption(v);
+                              }}
+                              className={Styles['option-badge']}
+                          >
+                              {v?.label}
+                              <span className={Styles['clear-btn']}>
+                                  &times;
+                              </span>
+                          </button>
+                      ))
+                    : value?.label}
+            </span>
             <button
                 onClick={(e) => {
                     e.stopPropagation();
